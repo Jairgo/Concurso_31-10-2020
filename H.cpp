d36e0762e61f92f148e778846c8b37c632b0c0c7
KiritoCJ <73753343+KiritoCJ@users.noreply.github.com>
2020-10-31 15:41:38 -0600

    Add files via upload

--- H.cpp
+++ H.cpp
@@ -1,44 +1,58 @@
-#include <iostream>
-#include <bits/stdc++.h> 
-using namespace std;
-int main() {
-    int t,n,x,sumaImp=0,sumaPar=0;
-    bool band = true; // inicia en impares
-    
-    cin >> t;
-
-    for (int i = 0; i < t; i++)
-    {
-        cin>> n;
-        int casa[n];
-        for (int j = 0; j < n; j++)
-        {
-            cin >> x;
-            casa[j]=x;
-        }
-        for (int j = 0; j < n; j++)
-        {
-            if(casa[j] != 0){
-                if(band){
-                    sumaImp +=casa[j];
-                    band = false;
-                }else{
-                    sumaPar += casa[j];
-                    band = true;
-                }
-            }
-        }
-
-
-        //if(sumaImp >= sumaPar ) cout << sumaImp<<'\n';
-        //else cout << sumaPar<<'\n';
-        cout << sumaImp <<'\n';
-        cout << sumaPar;
-        sumaImp=0;
-        sumaPar=0;
-        band = true;
-        
-    }
-    
-    return 0;
+#include<stdio.h>
+int A[1000];
+int main(){
+	int a=0,b=0,c=0,x=0,y=0,m=0,d=0;
+	scanf("%d",&a);
+	while(a>0){
+		scanf("%d",&b);
+		c=0;
+		while(c<b){
+			scanf("%d",&A[c]);
+			c++;
+		}
+		c=d=m=0;
+		
+		while(c<b){
+			if(d==0){
+				x=x+A[c];
+				d=1;
+			}
+			else{
+				y=y+A[c];
+				d=0;
+			}
+			c++;
+		}
+		if(x>y){
+			m=x;
+		}
+		else{
+			m=y;
+		}
+		c=d=x=y=0;
+		while(c<b){
+			if(d==0){
+				x=x+A[c];
+				if(A[c]!=0){
+					d=1;
+				}
+			}
+			else{
+				y=y+A[c];
+				if(A[c]!=0){
+					d=0;
+				}
+			}
+			c++;
+		}
+		if(x>y){
+			m=x;
+		}
+		else{
+			m=y;
+		}
+		printf("%d\n",m);
+		a--;
+	}
+return 0;
 }
