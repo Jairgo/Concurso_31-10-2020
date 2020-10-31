ec03865df5e515ae879e7a31f88576d446dfa65c
jairg_v <gomjair@gmail.com>
2020-10-31 13:09:06 -0600

    Checar h

--- HH.cpp
+++ HH.cpp
@@ -4,31 +4,26 @@ using namespace std;
 int main() {
     int t,n,x,sumaImp=0,sumaPar=0;
     bool band = true; // inicia en impares
-    
+    vector <int> casa;
     cin >> t;
 
     for (int i = 0; i < t; i++)
     {
         cin>> n;
-        int casa[n];
         for (int j = 0; j < n; j++)
         {
             cin >> x;
             casa[j]=x;
-        }
-        for (int j = 0; j < n; j++)
-        {
-            if(casa[j] != 0){
-                if(band){
-                    sumaImp +=casa[j];
-                    band = false;
-                }else{
-                    sumaPar += casa[j];
-                    band = true;
-                }
+            
+            if(x==0) band=!band;
+            if(band){
+                sumaImp+=x;
+                band = false;
+            }else{
+                sumaPar+=x;
+                band = true;
             }
         }
-
 
         //if(sumaImp >= sumaPar ) cout << sumaImp<<'\n';
         //else cout << sumaPar<<'\n';
